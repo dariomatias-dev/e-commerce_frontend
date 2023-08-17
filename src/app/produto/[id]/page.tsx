ecfd8fa15cb44c90e05c2b5c08e82f1ba57c67c8
dario <matiasdario75@gmail.com>
2023-08-17 13:56:55 -0300

    style(produto): made style adjustments and added information about payment options
    - Styling adjustments have been made to the product page to improve its presentation
    - Added more information about available payment options

--- src/app/produto/[id]/page.tsx
+++ src/app/produto/[id]/page.tsx
@@ -14,6 +14,7 @@ import { MdFavoriteBorder } from 'react-icons/md';
 import { ProductProps } from '@/@types/product';
 
 import { generateImageUrl } from '@/utils/generateImagePath';
+import { formatToReal } from '@/utils/formatToReal';
 
 type Props = {
   searchParams: Record<string, string>;
@@ -41,10 +42,24 @@ const Product = ({ searchParams }: Props) => {
 
   if (JSON.stringify(product) === '{}') return <></>;
 
+  const price = product.price;
+  const formattedPrice = formatToReal(price);
+
+  const valueInInstallments = price / 12;
+  const formattedValueInInstallments = formatToReal(
+    Number(valueInInstallments),
+  );
+
+  const oldPrice = price * 1.1;
+  const formattedOldPrice = formatToReal(oldPrice);
+
+  const discountPrice = price * 0.85;
+  const formattedDisccountPrice = formatToReal(discountPrice);
+
   return (
     <div className="flex flex-col gap-8 m-10">
-      <div className="flex justify-between bg-zinc-100 text-white p-10 rounded-md">
-        <div className="w-[500px]">
+      <div className="h-[500px] flex gap-20 bg-zinc-100 text-white p-10 rounded-md">
+        <div className="w-full">
           <div className="flex justify-end gap-4 mb-6">
             <BsShare className="w-6 h-6 text-gray-400 hover:text-gray-500 transition-all duration-300" />
             <MdFavoriteBorder className="w-6 h-6 text-gray-400 hover:text-gray-500 transition-all duration-300" />
@@ -55,7 +70,7 @@ const Product = ({ searchParams }: Props) => {
             effect="fade"
             loop={true}
             autoplay={true}
-            className="cursor-grab"
+            className="cursor-grab w-[300px]"
           >
             {Array.from({ length: product.amountOfImages }).map((_, index) => {
               const imageNumber = index + 1;
@@ -67,11 +82,11 @@ const Product = ({ searchParams }: Props) => {
 
               return (
                 <SwiperSlide key={index}>
-                  <div className="w-full h-full bg-zinc-100 p-32">
+                  <div className="w-full h-full bg-zinc-100 p-10">
                     <Image
                       src={imageUrl}
-                      width={500}
-                      height={500}
+                      width={1000}
+                      height={1000}
                       priority={true}
                       className="w-full h-auto object-contain"
                       alt={`Produto ${product.name}`}
@@ -86,7 +101,7 @@ const Product = ({ searchParams }: Props) => {
         <div className="w-full max-w-[580px] flex flex-col justify-between gap-4 bg-zinc-800 px-4 py-8 rounded-md">
           <div className="flex flex-col gap-5">
             <div className="flex flex-col gap-2">
-              <h1 className="text-2xl font-bold">{product.name}</h1>
+              <h1 className="text-3xl font-bold">{product.name}</h1>
               <p className="text-xs text-gray-400">Código: {product.id}</p>
               <p className="text-xs text-gray-300">
                 Vendido e entregue por:{' '}
@@ -94,15 +109,28 @@ const Product = ({ searchParams }: Props) => {
               </p>
             </div>
 
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-1">
+              <p className="text-sm text-zinc-400 font-semibold line-through">
+                {formattedPrice}
+              </p>
               <p className="text-green-500 text-2xl font-bold">
-                {Number(product.price).toLocaleString('pt-br', {
-                  style: 'currency',
-                  currency: 'BRL',
-                })}
+                {formattedDisccountPrice}
               </p>
               <p className="text-xs">
                 À vista no PIX ou boleto com 15% de desconto
+              </p>
+            </div>
+
+            <div>
+              <p className="text-sm text-zinc-200 font-bold">
+                {formattedOldPrice}
+              </p>
+              <p className="text-xs">
+                Em até 12x de{' '}
+                <span className="font-bold">
+                  {formattedValueInInstallments}
+                </span>{' '}
+                sem juros no cartão
               </p>
             </div>
           </div>
